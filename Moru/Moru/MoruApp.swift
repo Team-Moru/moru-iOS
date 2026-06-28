7940399077689bca2366a118d03ba041141635b1
민혁 <minhyeok@minhyeog-ui-Macmini.local>
2026-06-28 18:50:30 +0900

    chore: 초기 Xocde 프로젝트 세팅

---
+++ Moru/Moru/MoruApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoruApp.swift
+//  Moru
+//
+//  Created by 민혁 on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MoruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
